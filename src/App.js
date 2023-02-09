1e616a3110973ec76b209308ed592607e6ad5ac5
EderHdz <dariotecsoft90@gmail.com>
2023-02-08 20:22:52 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import './App.css';
 import freeCodeCampLogo from './imagenes/freecodecamp-logo.png'
+import Boton from './componentes/Boton';
 
 function App() {
+
+  const manejarClic = () => {
+    console.log('Clic');
+  }
+
+  const reiniciarContador = () => {
+    console.log('Reiniciar');
+  }
+
   return (
     <div className="App">
       <div className='freecodecamp-logo-contenedor'>
         <img className='freecodecamp-logo' 
         src={freeCodeCampLogo} 
         alt='Logo de freeCodeCamp' />
+      </div>
+      <div className='contenedor-principal'>
+        <Boton 
+        texto='Clic'
+        esBotonDeClic={true}
+        manejarClic={manejarClic}/> 
+        <Boton 
+        texto='reiniciar'
+        esBotonDeClic={false}
+        manejarClic={reiniciarContador}/> 
       </div>
     </div>
   );
